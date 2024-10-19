c4a5bce9a883d10ffd05aa5ec2da11cdd11b6e7d
Kevin Bryant <Kjbryant44@gmail.com>
2024-10-19 02:53:47 +0200

    try to fix the issues of recipes not loading

--- backend/server.js
+++ backend/server.js
@@ -43,7 +43,7 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   store: MongoStore.create({
-    mongoUrl: process.env.MONGODB_URI, // Make sure this environment variable is set in Render
+    mongoUrl: process.env.MONGODB_URI,
   }),
   cookie: {
     secure: process.env.NODE_ENV === 'production',
